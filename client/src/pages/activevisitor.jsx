ed55e5efc90f8d0c7b0c11babccf7bfab18b7e9f
rewa1437 <aarewa1437@gmail.com>
2025-07-14 16:21:34 +0700

    visitor active page-search bar and edit layout

--- client/src/pages/activevisitor.jsx
+++ client/src/pages/activevisitor.jsx
@@ -1,10 +1,26 @@
 
 import React from 'react';
+import '../css/atvt.css'; // Import the CSS file for styling
 
 const ActiveVisitor = () => {
   return (
-    <div className="main-container">
-      <div className="box-container">
+    <div className="main-container-def">
+      <div className="box-container-def">
+        <div className="search-box-def">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="size-6">
+            <path strokeLinecap="round" strokeLinejoin="round" d="m21 21-5.197-5.197m0 0A7.5 7.5 0 1 0 5.196 5.196a7.5 7.5 0 0 0 10.607 10.607Z" />
+          </svg>
+
+          <input
+            type="text"
+            placeholder="Search..."
+            className="search-input-def"
+          />
+        </div>
+        <div className="filter-box-def">
+          
+        </div>
+
         
       </div>
     </div>
